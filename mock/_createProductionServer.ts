f23d60aeac8db9ac5fc5b29220411ec83aabde4c
jia <1159063863@qq.com>
2026-04-10 15:55:47 +0800

    refactor: prune unused Naive UI components and register auth and dashboard mock servers

--- mock/_createProductionServer.ts
+++ mock/_createProductionServer.ts
@@ -1,6 +1,8 @@
 import { createProdMockServer } from 'vite-plugin-mock/client'
 import userMock from './user'
+import authMock from './auth'
+import dashboardMock from './dashboard'
 
 export function setupProdMockServer() {
-  createProdMockServer([...userMock])
+  createProdMockServer([...userMock, ...authMock, ...dashboardMock])
 }
